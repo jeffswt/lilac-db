794082c7a8d5523dcb68e54ad05d7379952d2ed4
Geoffrey Tang <jeffswt@users.noreply.github.com>
2022-02-23 15:05:09 +0800

    fix(keqing): auto create parent dirs in git-archive

--- keqing/rules/git/archive.mk
+++ keqing/rules/git/archive.mk
@@ -1,4 +1,5 @@
 # Exact archive of workspace at given commit
 
 $(ARTIFACTS)/git/archive/$(ARG_COMMIT_HASH).tar:
-	git archive --format tar --output="$(ARTIFACTS)/git/archive/$(ARG_COMMIT_HASH).tar" $(ARG_COMMIT_HASH)
+	mkdir --parents "$(ARTIFACTS)/git/archive/"
+	$(BIN_GIT) archive --format tar --output="$(ARTIFACTS)/git/archive/$(ARG_COMMIT_HASH).tar" "$(ARG_COMMIT_HASH)"
--- keqing/rules/keqing.mk
+++ keqing/rules/keqing.mk
@@ -1,5 +1,6 @@
 # Keqing must be run from the shell script. Running it directly through GNU
 # Make is not recommended.
+# Current working directory must be at repository root.
 
 # External arguments
 ARG_BRANCH      = $(_ARG_BRANCH)
@@ -7,11 +8,17 @@ ARG_COMMIT_HASH = $(_ARG_COMMIT_HASH)
 ARG_COMMIT_MSG  = $(_ARG_COMMIT_MSG)
 
 # All variable definitions
-KEQING          = ./keqing  # WARNING: ensure CWD at git root
+KEQING          = ./keqing
 ARTIFACTS       = $(KEQING)/.artifacts
 RULES           = $(KEQING)/rules
 
+# Applications
+BIN_BASH        = bash
+BIN_GIT         = git
+BIN_RUSTC       = rustc
+
 # Index of target rules
 include $(RULES)/git/archive.mk
+include $(RULES)/git/checkout.mk
 include $(RULES)/git/commit.mk
 include $(RULES)/git/workspace.mk
