27a3b49ad53e5068af6515ce938a63941116bb4a
Geoffrey Tang <jeffswt@users.noreply.github.com>
2022-02-28 18:21:19 +0800

    fix(keqing): add git autocrlf rule to setup script

--- keqing/rules/install/environ.mk
+++ keqing/rules/install/environ.mk
@@ -1,5 +1,11 @@
 
 $(ARTIFACTS)/install/environ:
+	@# commit LF, checkout LF
+	@"$(BIN_GIT)" config core.autocrlf input
+
+	@# install Rust
 	@echo "Select 'Proceed with installation (default)' on prompt."
 	@"$(BIN_CURL)" --proto '=https' --tlsv1.2 -sSf "https://sh.rustup.rs" | "$(BIN_BASH)"
+
+	@# add required Rust packages
 	@"$(BIN_RUSTUP)" component add rustfmt
