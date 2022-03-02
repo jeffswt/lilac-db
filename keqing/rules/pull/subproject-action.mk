e721ecb39725ab08b7dacb842bfd9983e3ab4fb6
Geoffrey Tang <jeffswt@users.noreply.github.com>
2022-03-02 23:55:36 +0800

    style(keqing): remove temporary subproject makefile example

--- keqing/rules/pull/subproject-action.mk
+++ keqing/rules/pull/subproject-action.mk
@@ -35,6 +35,3 @@
 #     either of them.
 
 __PULL__SUBPROJECT_ACTION = "$(BIN_BASH)" "$(RULES)/pull/subproject-action.sh" "$(ACTION_DEPENDS)" "$(ARTIFACTS)" "$(BIN_BASENAME)" "$(BIN_CARGO)" "$(BIN_CAT)" "$(BIN_CP)" "$(BIN_ECHO)" "$(BIN_FIND)" "$(BIN_GIT)" "$(BIN_MAKE)" "$(BIN_MKDIR)" "$(BIN_READLINK)" "$(BIN_RM)" "$(BIN_RUSTUP)" "$(BIN_TEE)" "$(BIN_TOUCH)" "$(RULES)"
-
-$(ARTIFACTS)/pull/build/workspace/flag:
-	@$(ACTION_SUBPROJ) build/release workspace
