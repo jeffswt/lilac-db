8df695664f7ece5cdf0c2a1476ca183af6aa82b6
Geoffrey Tang <jeffswt@users.noreply.github.com>
2022-03-03 15:43:41 +0800

    fix(keqing): grammar typo in clean rule

--- keqing/rules/misc/clean.mk
+++ keqing/rules/misc/clean.mk
@@ -2,4 +2,4 @@
 clean:
 	@"$(BIN_MKDIR)" --parents "$(ARTIFACTS)"
 	@"$(BIN_RM)" --recursive --verbose "$(ARTIFACTS)"
-	@"$(BIN_ECHO)" "All artifacts are clean successfully."
+	@"$(BIN_ECHO)" "All artifacts cleaned."
