cf842a4fa60fab8fb456669eda14625790458f2d
Geoffrey Tang <jeffswt@users.noreply.github.com>
2022-03-04 14:57:42 +0800

    style(keqing): add job start notifications

--- keqing/keqing.sh
+++ keqing/keqing.sh
@@ -20,7 +20,7 @@ keqing="make --file=\"./keqing/rules/keqing.mk\""
 # artifact path prefix
 artifacts="./keqing/.artifacts"
 
-# extract commit (returns as a global variable `$commit``)
+# extract commit (returns as a global variable `$commit`)
 function __resolve_commit() {
     set +e
     commit=$(git rev-parse "$1" 2>/dev/null)
@@ -32,10 +32,22 @@ function __resolve_commit() {
     fi
 }
 
-# extract branch name (returns as a global variable `$branch``)
+# extract commit message (returns as a global variable `$commit_msg`)
+function __resolve_commit_msg() {
+    set +e
+    commit_msg=$(git log "$commit" -1 --pretty=format:"%s")
+    retcode=$?
+    set -e
+    if [[ $retcode != 0 ]]; then
+        echo "fatal: unable to resolve commit message on '$commit'"
+        exit 1
+    fi
+}
+
+# extract branch name (returns as global variables `$branch` and `$commit`)
 function __resolve_branch() {
     set +e
-    git rev-parse "refs/heads/$1" 1>/dev/null 2>/dev/null
+    commit=$(git rev-parse "refs/heads/$1" 2>/dev/null)
     retcode=$?
     set -e
     if [[ $retcode != 0 ]]; then
@@ -48,6 +60,31 @@ function __resolve_branch() {
 # extracts current branch name
 function __show_current_branch() {
     branch=$(git branch --show-current)
+    commit=$(git rev-parse "refs/heads/$branch" 2>/dev/null)
+}
+
+# display current HEAD to user, possible arguments:
+#   - [description] 'workspace';
+#   - [description] 'commit'; [Global variables: $commit]
+#   - [description] 'branch'; [Global variables: $commit, $branch]
+function __echo_current_head() {
+    if [[ $2 == "workspace" ]]; then
+        echo "$1 started on"
+        echo "  - $cwd"
+        echo "----------------------------------------------------------------"
+    elif [[ $2 == "commit" ]]; then
+        __resolve_commit_msg
+        echo "$1 started on"
+        echo "  - ${commit:0:8} $commit_msg"
+        echo "----------------------------------------------------------------"
+    elif [[ $2 == "branch" ]]; then
+        __resolve_commit_msg
+        echo "$1 started on"
+        echo "  - $branch@${commit:0:8} $commit_msg"
+        echo "----------------------------------------------------------------"
+    else
+        exit 128
+    fi
 }
 
 ###############################################################################
@@ -59,16 +96,20 @@ if [[ "$1 $2" == "help " ]]; then
 
 # release related
 elif [[ "$1 $2;$3" == "build release;" ]]; then
+    __echo_current_head "Release build" workspace
     eval $keqing "$artifacts/build/release/workspace"
 elif [[ "$1 $2 $3 $4 ... ;$6" == "build release on commit ... ;" ]]; then
     __resolve_commit "$5"
+    __echo_current_head "Release build" commit
     eval $keqing "$artifacts/build/release/commit/$commit"
 elif [[ "$1 $2 $3 $4 ... ;$6" == "build release on branch ... ;" ]]; then
     __resolve_branch "$5"
+    __echo_current_head "Release build" branch
     eval $keqing "$artifacts/build/release/branch/$branch"
 
 # autofix
 elif [[ "$1 $2;$3" == "fix styles;" ]]; then
+    __echo_current_head "Code style fix" workspace
     eval $keqing "$artifacts/fix/style/workspace"
 
 # repository actions
@@ -92,35 +133,45 @@ elif [[ "$1;$2" == "clean;" ]]; then
 # pull request related
 elif [[ "$1 $2 $3 ... ;$5" == "merge with message ... ;" ]]; then
     __show_current_branch
+    __echo_current_head "Code check-in" branch
     eval $keqing "$artifacts/pull/merge/$branch" _ARG_COMMIT_MSG="$4"
 elif [[ "$1 $2 $3;$4" == "run pr gate;" ]]; then
     __show_current_branch
+    __echo_current_head "Code check-in validation" branch
     eval $keqing "$artifacts/pull/pr-gate/$branch"
 
 # styles
 elif [[ "$1 $2 $3;$4" == "check commit messages;" ]]; then
     __show_current_branch
+    __echo_current_head "Commit message style checks" branch
     eval $keqing "$artifacts/style/branch-commit-msgs/$branch"
 elif [[ "$1 $2 $3 $4 $5 ... ;$7" == "check commit messages on branch;" ]]; then
     __resolve_branch "$6"
+    __echo_current_head "Commit message style checks" branch
     eval $keqing "$artifacts/style/branch-commit-msgs/$branch"
 elif [[ "$1 $2;$3" == "check styles;" ]]; then
+    __echo_current_head "Code style checks" workspace
     eval $keqing "$artifacts/style/lint/workspace"
 elif [[ "$1 $2 $3 $4 ... ;$6" == "check styles on commit ... ;" ]]; then
     __resolve_commit "$5"
+    __echo_current_head "Code style checks" commit
     eval $keqing "$artifacts/style/lint/commit/$commit"
 elif [[ "$1 $2 $3 $4 ... ;$6" == "check styles on branch ... ;" ]]; then
     __resolve_branch "$5"
+    __echo_current_head "Code style checks" branch
     eval $keqing "$artifacts/style/lint/branch/$branch"
 
 # tests
 elif [[ "$1 $2 $3;$4" == "run unit tests;" ]]; then
+    __echo_current_head "Tests" workspace
     eval $keqing "$artifacts/test/unit/workspace"
 elif [[ "$1 $2 $3 $4 $5 ... ;$7" == "run unit tests on commit ... ;" ]]; then
     __resolve_commit "$6"
+    __echo_current_head "Tests" commit
     eval $keqing "$artifacts/test/unit/commit/$commit"
 elif [[ "$1 $2 $3 $4 $5 ... ;$7" == "run unit tests on branch ... ;" ]]; then
     __resolve_branch "$6"
+    __echo_current_head "Tests" branch
     eval $keqing "$artifacts/test/unit/branch/$branch"
 
 # invalid arguments
