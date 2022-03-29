c54beb58a682ef2f46d7e8e4f63d41ae72f83300
Geoffrey Tang <jeffswt@users.noreply.github.com>
2022-03-29 22:57:48 +0800

    fix(kleestor): unify benchmark scripts

---
+++ kleestor/src/benchmark/mod.rs
@@ -0,0 +1,100 @@
+use json::{object, JsonValue};
+use std::fs::File;
+use std::io::{Result, Write};
+
+mod nstree;
+
+/// Result of one benchmark run.
+pub struct BenchmarkResult {
+    title: String,
+    data: Vec<DataPoint>,
+}
+
+impl BenchmarkResult {
+    pub fn to_json(&self) -> JsonValue {
+        object! {
+            "title" => String::from(&self.title),
+            "data" => JsonValue::Array(self.data.iter().map(|item| item.to_json()).collect()),
+        }
+    }
+}
+
+/// An (x, y) data point pair.
+pub struct DataPoint {
+    x: f64,
+    y: f64,
+}
+
+impl DataPoint {
+    pub fn to_json(&self) -> JsonValue {
+        object! {
+            "x" => self.x,
+            "y" => self.y,
+        }
+    }
+}
+
+/// Facade of getting all results.
+pub struct BenchmarkManager {
+    results: Vec<BenchmarkResult>,
+}
+
+/// Execute all benchmarks and export to file.
+///
+/// Usage: `BenchmarkManager::run(...path)`.
+#[allow(dead_code)]
+impl BenchmarkManager {
+    pub fn run(path: &str) -> () {
+        Self { results: vec![] }.execute(path)
+    }
+
+    /// This function contains a list of benchmark items to run.
+    fn execute(&mut self, path: &str) -> () {
+        self.add(nstree::btreebuiltin_rand_rw());
+        self.add(nstree::btreebuiltin_seq_rw());
+        self.add(nstree::btreeimpl_rand_rw::<3>());
+        self.add(nstree::btreeimpl_seq_rw::<3>());
+        self.add(nstree::btreeimpl_rand_rw::<5>());
+        self.add(nstree::btreeimpl_seq_rw::<5>());
+        self.add(nstree::btreeimpl_rand_rw::<6>());
+        self.add(nstree::btreeimpl_seq_rw::<6>());
+        self.add(nstree::btreeimpl_rand_rw::<7>());
+        self.add(nstree::btreeimpl_seq_rw::<7>());
+        self.add(nstree::btreeimpl_rand_rw::<8>());
+        self.add(nstree::btreeimpl_seq_rw::<8>());
+        self.add(nstree::btreeimpl_rand_rw::<9>());
+        self.add(nstree::btreeimpl_seq_rw::<9>());
+        self.add(nstree::btreeimpl_rand_rw::<11>());
+        self.add(nstree::btreeimpl_seq_rw::<11>());
+        self.add(nstree::btreeimpl_rand_rw::<13>());
+        self.add(nstree::btreeimpl_seq_rw::<13>());
+        self.add(nstree::rbtree_rand_rw());
+        self.add(nstree::rbtree_seq_rw());
+        self.add(nstree::btreeunsafe_rand_rw::<7>());
+        self.add(nstree::btreeunsafe_seq_rw::<7>());
+        self.add(nstree::splay_rand_rw());
+        self.add(nstree::splay_seq_rw());
+
+        self.save(path).unwrap();
+    }
+
+    /// Add records to the result.
+    fn add(&mut self, results: Vec<BenchmarkResult>) -> () {
+        for result in results {
+            let title = &result.title;
+            let len = result.data.len();
+            println!("'{title}' : {len} entries");
+            self.results.push(result);
+        }
+    }
+
+    /// Save results to file.
+    fn save(&mut self, path: &str) -> Result<()> {
+        let obj = JsonValue::Array(self.results.iter().map(|result| result.to_json()).collect());
+        let doc = obj.pretty(2);
+        // write stuff
+        let mut file = File::create(path)?;
+        file.write(&doc.as_bytes())?;
+        Ok(())
+    }
+}
---
+++ kleestor/src/benchmark/nstree.rs
@@ -0,0 +1,185 @@
+use std::time::Instant;
+
+use crate::memtable::btree::BTreeImpl;
+use crate::memtable::btree_builtin::BTreeBuiltin;
+use crate::memtable::btree_unsafe::BTreeUnsafe;
+use crate::memtable::rbtree::RBTree;
+use crate::memtable::splay::SplayTree;
+use crate::memtable::MemTable;
+
+use super::{BenchmarkResult, DataPoint};
+
+enum TestMode {
+    Random,
+    Sequential,
+}
+
+/// Run I/O performance benchmarks on n-ary search trees.
+fn run(
+    mut map: Box<dyn MemTable<i64, i64>>,
+    read_title: &str,
+    write_title: &str,
+    test_mode: TestMode,
+) -> Vec<BenchmarkResult> {
+    // each datapoint will contain `scale` transactions
+    let scale = 20000_i64;
+    let iterations = 500_i64;
+
+    // baseline time consumption
+    let baseline_loop = Instant::now();
+    let mut _counter: u64 = 0;
+    for _ in 0..scale {
+        _counter = (_counter * 3 + 1) & 0x0fffffffffffffff;
+    }
+    let baseline_loop = baseline_loop.elapsed().as_nanos();
+
+    // initialize containers
+    let mut read_result = BenchmarkResult {
+        title: read_title.to_string(),
+        data: vec![],
+    };
+    let mut write_result = BenchmarkResult {
+        title: write_title.to_string(),
+        data: vec![],
+    };
+
+    // run iterations
+    let upper_bound = scale * iterations;
+    for iteration in 1..=iterations {
+        // evaluate write speed
+        let loop_time = Instant::now();
+        for i in (iteration - 1) * scale..iteration * scale {
+            let key = match test_mode {
+                TestMode::Random => (i * 921544879) % upper_bound,
+                TestMode::Sequential => i,
+            };
+            let value = key * 2 + 1;
+            map.as_mut().insert(key, value);
+        }
+        let loop_time = loop_time.elapsed().as_nanos() - baseline_loop;
+        write_result.data.push(DataPoint {
+            x: (iteration * scale) as f64,
+            y: (loop_time as f64) / 1.0e9,
+        });
+
+        // evaluate read speed
+        let loop_time = Instant::now();
+        for i in (iteration - 1) * scale..iteration * scale {
+            let key = match test_mode {
+                TestMode::Random => (i * 921544879) % upper_bound,
+                TestMode::Sequential => i,
+            };
+            let value = match map.as_mut().get(&key) {
+                Some(&mut x) => x,
+                None => -1,
+            };
+            // assert!(value == key * 2 + 1);
+        }
+        let loop_time = loop_time.elapsed().as_nanos() - baseline_loop;
+        read_result.data.push(DataPoint {
+            x: (iteration * scale) as f64,
+            y: (loop_time as f64) / 1.0e9,
+        });
+    }
+
+    // collect
+    vec![read_result, write_result]
+}
+
+pub fn splay_rand_rw() -> Vec<BenchmarkResult> {
+    run(
+        Box::from(SplayTree::new()),
+        "memtable-splay-rand-read",
+        "memtable-splay-rand-write",
+        TestMode::Random,
+    )
+}
+
+pub fn splay_seq_rw() -> Vec<BenchmarkResult> {
+    run(
+        Box::from(SplayTree::new()),
+        "memtable-splay-seq-read",
+        "memtable-splay-seq-write",
+        TestMode::Sequential,
+    )
+}
+
+pub fn rbtree_rand_rw() -> Vec<BenchmarkResult> {
+    run(
+        Box::from(RBTree::new()),
+        "memtable-rbtree-rand-read",
+        "memtable-rbtree-rand-write",
+        TestMode::Random,
+    )
+}
+
+pub fn rbtree_seq_rw() -> Vec<BenchmarkResult> {
+    run(
+        Box::from(RBTree::new()),
+        "memtable-rbtree-seq-read",
+        "memtable-rbtree-seq-write",
+        TestMode::Sequential,
+    )
+}
+
+pub fn btreebuiltin_rand_rw() -> Vec<BenchmarkResult> {
+    run(
+        Box::from(BTreeBuiltin::new()),
+        "memtable-btree_builtin-rand-read",
+        "memtable-btree_builtin-rand-write",
+        TestMode::Random,
+    )
+}
+
+pub fn btreebuiltin_seq_rw() -> Vec<BenchmarkResult> {
+    run(
+        Box::from(BTreeBuiltin::new()),
+        "memtable-btree_builtin-seq-read",
+        "memtable-btree_builtin-seq-write",
+        TestMode::Sequential,
+    )
+}
+
+pub fn btreeunsafe_rand_rw<const N: usize>() -> Vec<BenchmarkResult> {
+    run(
+        Box::from(BTreeUnsafe::<i64, i64, N>::new()),
+        &format!("memtable-btree_unsafe_{N}-rand-read"),
+        &format!("memtable-btree_unsafe_{N}-rand-write"),
+        TestMode::Random,
+    )
+}
+
+pub fn btreeunsafe_seq_rw<const N: usize>() -> Vec<BenchmarkResult> {
+    run(
+        Box::from(BTreeUnsafe::<i64, i64, N>::new()),
+        &format!("memtable-btree_unsafe_{N}-seq-read"),
+        &format!("memtable-btree_unsafe_{N}-seq-write"),
+        TestMode::Sequential,
+    )
+}
+
+pub fn btreeimpl_rand_rw<const N: usize>() -> Vec<BenchmarkResult>
+where
+    [(); N * 2]: Sized,
+    [(); N * 2 + 1]: Sized,
+{
+    run(
+        Box::from(BTreeImpl::<i64, i64, N>::new()),
+        &format!("memtable-btree_{N}-rand-read"),
+        &format!("memtable-btree_{N}-rand-write"),
+        TestMode::Random,
+    )
+}
+
+pub fn btreeimpl_seq_rw<const N: usize>() -> Vec<BenchmarkResult>
+where
+    [(); N * 2]: Sized,
+    [(); N * 2 + 1]: Sized,
+{
+    run(
+        Box::from(BTreeImpl::<i64, i64, N>::new()),
+        &format!("memtable-btree_{N}-seq-read"),
+        &format!("memtable-btree_{N}-seq-write"),
+        TestMode::Sequential,
+    )
+}
