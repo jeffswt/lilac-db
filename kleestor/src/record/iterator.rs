7d056190652e3a083d44a99cdcc92d5f07feffb5
Geoffrey Tang <jeffswt@users.noreply.github.com>
2022-05-23 00:57:05 +0800

    docs(kleestor): update kvpointer lifetime notes

--- kleestor/src/record/iterator.rs
+++ kleestor/src/record/iterator.rs
@@ -1,21 +1,27 @@
-use crate::record::KvDataRef;
-
-use super::KvEntry;
+use crate::record::{KvDataRef, KvEntry};
 
 /// Key-value iterator (pointer) interface.
 pub trait KvPointer {
     /// Get key where iterator points to.
+    ///
+    /// It should be reminded that the resulting reference must live as long as
+    /// the data structure does.
     fn key(&self) -> &[u8];
 
     /// Gets a reference to the pointing value.
     ///
-    /// You should expect this reference to invalidate as soon as the pointer
-    /// left this region.
+    /// This reference should try to live as long as the data structure does,
+    /// given that the value itself was not overwritten. Therefore additional
+    /// measures (e.g. MVCC, locks) must be taken so that the value is not
+    /// freed while it is in the hands of another iterator. 
     fn value(&self) -> KvDataRef;
 
     /// Gets a mutable reference to the pointing value.
     ///
-    /// This exposes the underlying implementation. Lifetime should be manually
-    /// ensured.
+    /// This exposes the underlying implementation. Expect reference to
+    /// invalidate after pointer leaves scope.
+    ///
+    /// This function may be unimplemented since certain data structures are
+    /// opened readonly.
     fn value_mut(&self) -> &mut KvEntry;
 }
