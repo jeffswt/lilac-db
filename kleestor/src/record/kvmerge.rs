eeefa06635c497d90be516f86f9daa627dda1c57
Geoffrey Tang <jeffswt@users.noreply.github.com>
2022-05-23 18:11:13 +0800

    fix(sstable): priority not handled well

--- kleestor/src/record/kvmerge.rs
+++ kleestor/src/record/kvmerge.rs
@@ -52,14 +52,19 @@ where
             };
             let key = unsafe { utils::reborrow_slice(item.key()) };
 
-            let item_s = item._to_string();
-            println!("binary insert -- {item_s}");
-
-            let insert_at = match self.binary_search(key) {
-                FoundIndex::Equal(_) => continue,
-                FoundIndex::Less(idx) => idx,
+            match self.binary_search(key) {
+                FoundIndex::Equal(insert_at) => {
+                    // higher priority
+                    if index < self.buffer[insert_at].2 {
+                        self.buffer[insert_at] = (key, item, index);
+                    } else {
+                        continue;
+                    }
+                }
+                FoundIndex::Less(insert_at) => {
+                    self.buffer.insert(insert_at, (key, item, index));
+                }
             };
-            self.buffer.insert(insert_at, (key, item, index));
             break;
         }
     }
@@ -143,4 +148,74 @@ enum FoundIndex {
     Less(usize),
     /// The discovered [`vec[index]`] is equal to the queried key.
     Equal(usize),
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::memtable::rbtree::RBTree;
+    use crate::memtable::MemTable;
+    use crate::record::{ByteStream, KvData, KvEntry, KvPointer};
+    use crate::sstable::reader::SSTableReader;
+    use crate::sstable::writer::SSTableWriter;
+    use std::io::Result;
+    use std::path::PathBuf;
+
+    use super::KvMergeIterator;
+
+    #[test]
+    fn merge_ok() -> () {
+        // write 1000 - 9999 in 4 files to modulo separately
+        create_run(0, 1000, 9999, 4).unwrap();
+        create_run(1, 1001, 9999, 4).unwrap();
+        create_run(2, 1002, 9999, 4).unwrap();
+        create_run(3, 1003, 9999, 4).unwrap();
+        // load runs
+        let runs = vec![read_run(0), read_run(1), read_run(2), read_run(3)];
+        let iters = runs.iter().map(|run| run.iter()).collect();
+        // read merged items
+        let merger = KvMergeIterator::new(iters);
+        for item in merger {
+            let z = item._to_string();
+            println!("found -- {z}");
+        }
+        // clean objects
+        for id in 0..=3 {
+            let _ = std::fs::remove_file(&get_file_path(id));
+        }
+    }
+
+    fn get_file_path(id: u32) -> PathBuf {
+        let mut tmp_dir = std::env::temp_dir();
+        tmp_dir.push(format!("_kleestor_record_kvmerge_run_{id}.db"));
+        let s = tmp_dir.to_str().unwrap();
+        tmp_dir
+    }
+
+    /// Write run #id to disk.
+    fn create_run(id: u32, begin: i64, end: i64, skip: usize) -> Result<()> {
+        // create input memtable
+        let mut map = RBTree::<ByteStream, KvEntry>::new();
+        for i in (begin..=end).step_by(skip) {
+            let key = format!("sample-key-{i}");
+            let value = format!("value-{i}-{i}-{i}-{i}-run-{id}");
+            map.insert(
+                ByteStream::from_slice(key.as_bytes()),
+                KvEntry::new(KvData::Value {
+                    cached: false,
+                    value: ByteStream::from_slice(value.as_bytes()),
+                }),
+            );
+        }
+        // write data to disk
+        let file = std::fs::File::create(&get_file_path(id))?;
+        let mut table = SSTableWriter::new(file);
+        table.write(map.iter_mut())?;
+        Ok(())
+    }
+
+    /// Read run from disk.
+    fn read_run(id: u32) -> SSTableReader {
+        let file = std::fs::File::open(&get_file_path(id)).unwrap();
+        SSTableReader::new(file).unwrap()
+    }
 }
